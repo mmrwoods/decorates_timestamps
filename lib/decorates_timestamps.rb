0145262268868e5856b4834d8e03fb8f077078a0
thickpaddy <mwoods@online.ie>
2013-06-01 09:51:23 +0100

    Refactoring, mostly of specs

--- lib/decorates_timestamps.rb
+++ lib/decorates_timestamps.rb
@@ -8,7 +8,6 @@ require 'i18n'
 # * Test with draper v1.x (should work, but needs to be tested)
 
 module DecoratesTimestamps
-
   class << self
     def default_timestamp_decorator
       I18nTimestampDecorator
@@ -26,27 +25,29 @@ module DecoratesTimestamps
     end
   end
 
-  def decorates_timestamp(timestamp_symbol, options={})
-    define_method(timestamp_symbol) do
-      attribute_value = model.send(timestamp_symbol)
-      return if attribute_value.nil?
-      DecoratesTimestamps.default_timestamp_decorator.new(attribute_value, options)
+  module ClassMethods
+    def decorates_timestamp(timestamp_symbol, options={})
+      define_method(timestamp_symbol) do
+        attribute_value = model.send(timestamp_symbol)
+        return if attribute_value.nil?
+        DecoratesTimestamps.default_timestamp_decorator.new(attribute_value, options)
+      end
     end
-  end
 
-  def decorates_timestamps(options={})
-    timestamp_symbols.each{|sym| decorates_timestamp(sym, options) }
-  end
+    def decorates_timestamps(options={})
+      timestamp_symbols.each{|sym| decorates_timestamp(sym, options) }
+    end
 
-  private
-  def timestamp_symbols
-    self.model_class.column_names.select{|name|
-      name.match(/_(at|on)$/)
-    }.map(&:to_sym)
+    private
+    def timestamp_symbols
+      self.model_class.column_names.select{|name|
+        name.match(/_(at|on)$/)
+      }.map(&:to_sym)
+    end
   end
 end
 
 module Draper
   # note: Draper::Base replaced with Draper::Decorator in v1.x
-  ( defined?(Base) ? Base : Decorator ).extend(DecoratesTimestamps)
+  ( defined?(Base) ? Base : Decorator ).extend(DecoratesTimestamps::ClassMethods)
 end
--- spec/decorates_timestamps_spec.rb
+++ spec/decorates_timestamps_spec.rb
@@ -1,9 +1,13 @@
 require 'spec_helper'
 
 module Draper
-  describe "Base decorator" do
-    # note: Draper::Base replaced with Draper::Decorator in v1.x
-    subject { defined?(Base) ? Base : Decorator }
+
+  # note: Draper::Base replaced with Draper::Decorator in v1.x
+  base_decorator = defined?(Base) ? Base : Decorator
+
+  describe base_decorator do
+
+    subject { base_decorator  }
 
     it "should respond to decorates_timestamp" do
       subject.should respond_to(:decorates_timestamp)
@@ -12,108 +16,117 @@ module Draper
     it "should respond to decorates_timestamps" do
       subject.should respond_to(:decorates_timestamps)
     end
+
   end
+
 end
 
 module DecoratesTimestamps
+
   describe I18nTimestampDecorator do
+
+    let(:value) { "foo" }
+    let(:options) { {bar: :baz} }
+
+    subject { I18nTimestampDecorator.new(value, options) }
+
     describe "#to_s" do
-
-      before(:all) do
-        @value = Time.now
-        @options = {:foo => :bar}
-        @decorator = I18nTimestampDecorator.new(@value, @options)
-      end
-
       it "should localize the input" do
-        I18n.should_receive(:localize).with(@value, anything)
-        @decorator.to_s
+        I18n.should_receive(:localize).with(value, anything)
+        subject.to_s
       end
 
       it "should pass any provided options to I18n.localize" do
-        I18n.should_receive(:localize).with(@value, @options)
-        @decorator.to_s
+        I18n.should_receive(:localize).with(value, options)
+        subject.to_s
       end
     end
+
   end
-end
 
-describe TestDecorator do
+  describe ClassMethods do
 
-  describe ".decorates_timestamp" do
+    subject { TestDecorator.new(TestModel.new) }
 
-    let(:object) { TestModel.new }
-    let(:decorator) { TestDecorator.new(object) }
+    describe ".decorates_timestamp" do
 
-    it "should define a decorator method for the attribute" do
-      TestDecorator.decorates_timestamp(:foo)
-      decorator.should respond_to(:foo)
-    end
-
-    describe "decorator method" do
-
-      context "when source attribute value is nil" do
-        before(:all) do
-          TestModel.class_eval { def foo; nil; end}
-        end
-
-        it "should return nil" do
-          decorator.foo.should == nil
-        end
+      it "should define a decorator method for the attribute" do
+        TestDecorator.decorates_timestamp(:foo)
+        subject.should respond_to(:foo)
       end
 
-      context "when source attribute has some value" do
-        before(:all) do
-          TestModel.class_eval { def foo; "foo"; end}
-          @default_decorator = DecoratesTimestamps.default_timestamp_decorator
-        end
+      describe "decorator method" do
 
-        it "should decorate the value with the default decorator" do
-          @default_decorator.should_receive(:new).with("foo", anything)
-          decorator.foo
-        end
+        context "when source attribute value is nil" do
+          before(:all) do
+            TestModel.class_eval { def foo; nil; end}
+          end
 
-        it "should return an instance of the default decorator" do
-          decorator.foo.class.should == @default_decorator
-        end
-
-        context "and options were provided when creating the method" do
-          it "should provide the same options to the decorator" do
-            options = {:bar => :baz}
-            TestDecorator.decorates_timestamp(:foo, options)
-            @default_decorator.should_receive(:new).with("foo", options)
-            decorator.foo
+          it "should return nil" do
+            subject.foo.should == nil
           end
         end
-      end
 
-    end
+        context "when source attribute has some value" do
 
-  end
+          let(:default_timestamp_decorator) {
+            DecoratesTimestamps.default_timestamp_decorator
+          }
 
-  describe ".decorates_timestamps" do
+          before(:all) do
+            TestModel.class_eval { def foo; "foo"; end}
+          end
 
-    before(:all) do
-      @timestamp_symbols = TestModel.column_names.select{|name|
-        name.match(/_(at|on)$/)
-      }
-    end
+          it "should decorate the value with the default decorator" do
+            default_timestamp_decorator.should_receive(:new).with("foo", anything)
+            subject.foo
+          end
 
-    it "should call decorates_timestamp for each timestamp" do
-      @timestamp_symbols.each do |sym|
-        TestDecorator.should_receive(:decorates_timestamp).with(sym, anything)
-      end
-      TestDecorator.decorates_timestamps
-    end
+          it "should return an instance of the default decorator" do
+            subject.foo.class.should == default_timestamp_decorator
+          end
 
-    context "with an options argument" do
-      it "should provide the options to decorates_timestamp" do
-        options = {:bar => :baz}
-        @timestamp_symbols.each do |sym|
-          TestDecorator.should_receive(:decorates_timestamp).with(sym, options)
+          context "and options were provided when creating the method" do
+            it "should provide the same options to the decorator" do
+              options = {:bar => :baz}
+              TestDecorator.decorates_timestamp(:foo, options)
+              default_timestamp_decorator.should_receive(:new).with("foo", options)
+              subject.foo
+            end
+          end
+
         end
-        TestDecorator.decorates_timestamps(options)
+
       end
+
+    end
+
+    describe ".decorates_timestamps" do
+
+      let(:timestamp_symbols) {
+        TestModel.column_names.select{|name| name.match(/_(at|on)$/) }
+      }
+
+      it "should call decorates_timestamp for each timestamp" do
+        timestamp_symbols.each do |sym|
+          TestDecorator.should_receive(:decorates_timestamp).with(sym, anything)
+        end
+        TestDecorator.decorates_timestamps
+      end
+
+      context "with an options argument" do
+
+        let(:options) { {bar: :baz} }
+
+        it "should provide the options to decorates_timestamp" do
+          timestamp_symbols.each do |sym|
+            TestDecorator.should_receive(:decorates_timestamp).with(sym, options)
+          end
+          TestDecorator.decorates_timestamps(options)
+        end
+
+      end
+
     end
 
   end
